6deab63e283308d5a46044b1dded747f70940e03
maksi <maksi@DESKTOP-9D3196E>
2022-04-05 00:31:26 +0500

    init

---
+++ ChatVaadin/src/main/java/max/ChatVaadinApplication.java
@@ -0,0 +1,13 @@
+package max;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatVaadinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatVaadinApplication.class, args);
+	}
+
+}
